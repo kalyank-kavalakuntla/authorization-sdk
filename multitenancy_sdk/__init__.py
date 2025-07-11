b62eb44beb2adf05602ce0dfb2cb095af6566f31
kalyank-kavalakuntla <kalyank9939@gmail.com>
2025-07-11 12:20:33 +0530

    issues fixed

--- multitenancy_sdk/__init__.py
+++ multitenancy_sdk/__init__.py
@@ -1,5 +1,4 @@
 from .client import AuthClient
 from .decorators import requires_auth
-from . import config
 
-__all__ = ['AuthClient', 'requires_auth', 'config']
+__all__ = ['AuthClient', 'requires_auth']
